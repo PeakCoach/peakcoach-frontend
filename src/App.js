b6c5cb4f73776998f7935355804b28161764bd86
CoachBasil <coachbasil97@gmail.com>
2026-05-06 03:04:38 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'Arial' }}>
+      <h1>مرحباً بك في Peak Coach</h1>
+      <p>يتم الآن بناء منصة التدريب الخاصة بك...</p>
+    </div>
+  );
+}
+
+export default App;
